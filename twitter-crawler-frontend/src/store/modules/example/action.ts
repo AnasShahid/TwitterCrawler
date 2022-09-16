3d5a5ba9487665d5fea2d267b65453f079d9b3b0
hqadeer@nisum.com <nisum123>
2022-09-16 17:22:02 +0500

    conflicts resolved

--- twitter-crawler-frontend/src/store/modules/example/action.ts
+++ twitter-crawler-frontend/src/store/modules/example/action.ts
@@ -1,11 +1,11 @@
 import { ACTION_TYPES } from "./types";
 export const incrementCounter = () => {
   return {
-    type: ACTION_TYPES.EXAMPLE_INCREMENT_COUNTER,
+    type: ACTION_TYPES.EXAMPLE_INCREAMENT_COUNTER,
   };
 };
 export const decrementCounter = () => {
   return {
-    type: ACTION_TYPES.EXAMPLE_DECREMENT_COUNTER,
+    type: ACTION_TYPES.EXAMPLE_DECREAMENT_COUNTER,
   };
 };
